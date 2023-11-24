68797aff0018b4e6df6f143df8e90fd4c014bc90
Muas09 <116275467+Muas09@users.noreply.github.com>
2023-11-24 14:00:27 +0700

    update search function

---
+++ components/Home/Data.js
@@ -0,0 +1,98 @@
+
+const Data = {
+    products: [
+        {
+            id: 1,
+            image: require('../../assets/Home/Rest-circle.png'),
+            nameProduct: "Vegan Resto",
+            location: '19 km',
+            rating: '4.8',
+            min: '12 min',
+            description: "Most whole Alaskan Red King Crabs get broken down into legs, claws, and lump meat. We offer all of these options as well in our online shop, but there is nothing like getting the whole . . . .",
+        },
+        {
+            id: 2,
+            nameProduct: "Healthy Resto",
+            image: require('../../assets/Home/Heaththy.png'),
+            location: '21 km',
+            rating: '4.9',
+            min: '13 min',
+            description: "Whole Alaskan Red King Crabs are often broken down into legs, claws, and lump meat. You can find all these options in our online store, but there's a unique experience in having the whole crab delivered to your door."
+        },
+        {
+            id: 3,
+            nameProduct: "Good Food",
+            image: require("../../assets/Home/luamach.png"),
+            min: '14 min',
+            location: '9 km',
+            rating: '5.0',
+            description: "The majority of Alaskan Red King Crabs are separated into legs, claws, and lump meat. We offer all of these choices in our online store, but there's something special about ordering the whole crab...",
+
+        },
+        {
+            id: 4,
+            nameProduct: "Vegan Resto",
+            image: require("../../assets/Home/Rest-circle.png"),
+            min: '12 min',
+            location: '10 km',
+            rating: '5.1',
+            description: "Most Alaskan Red King Crabs are categorized into parts like legs, claws, and lump meat. At our online store, you can choose each type individually, but nothing compares to ordering the whole crab...",
+
+        },
+        {
+            id: 5,
+            nameProduct: "Healthy Resto",
+            image: require("../../assets/Home/Rest-circle.png"),
+            min: '13 min',
+            location: '20 km',
+            rating: '5.2',
+            description: "Alaskan Red King Crabs are typically divided into legs, claws, and lump meat. We provide all these options in our online shop, but there's no substitute for ordering the whole crab...",
+
+        },
+        {
+            id: 6,
+            nameProduct: "Healthy Resto",
+            image: require("../../assets/Home/Rest-circle.png"),
+            min: '13 min',
+            location: '20 km',
+            rating: '5.4',
+            description: "Most whole Alaskan Red King Crabs are typically divided into legs, claws, and lump meat. While we offer all of these options in our online shop, there's nothing quite like getting the whole crab...",
+
+        },
+    ],
+    menus: [
+        {
+            id: 1,
+            nameMenu: "Herbar Pancake",
+            name: 'Wijie Resto',
+            rating: '4.8',
+            order: '2000 +',
+            image: require("../../assets/Home/MenuPhoto.png"),
+            description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+            price: 45,
+        },
+        {
+            id: 2,
+            nameMenu: "Fruit Salad",
+            name: 'Wijie Resto',
+            rating: '4.8',
+            order: '3000 +',
+            image: require("../../assets/Home/Menu.png"),
+            description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+            price: 50,
+        },
+        {
+            id: 3,
+            nameMenu: "Green Noddle",
+            name: 'Noodle Home',
+            rating: '5.8',
+            order: '4000 +',
+            image: require("../../assets/Home/Photo.png"),
+            description: "Nulla occaecat velit laborum exercitation ullamco. Elit labore eu aute elit nostrud culpa velit excepteur deserunt sunt. Velit non est cillum consequat cupidatat ex Lorem laboris labore aliqua ad duis eu laborum.",
+            price: 67,
+        },
+    ],
+};
+
+export default Data;
+
